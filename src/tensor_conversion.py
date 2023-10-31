34709b8713fa066be348efba7ce0eea2fe5a0c26
usin8611 <usin8611@apyf95w5jw6.modem>
2023-10-31 17:59:41 +1100

    modified tensor_conversion with 3*3*3*3 instead of 4*4*4*4. In tensor_rotation, there is a minor issue, the phi1 and phi2 should be swiped

--- src/tensor_conversion.py
+++ src/tensor_conversion.py
@@ -1,4 +1,4 @@
-# function that converts 4th rank stiffness tensor to Voigt matrix notation
+# Define a function to convert 3x3x3x3 stiffness tensor to Voigt matrix notation
 def tensor_to_voigt(tensor):
     # Initialize an empty 6x6 matrix
     voigt = [[0 for i in range(6)] for j in range(6)]
@@ -14,13 +14,13 @@ def tensor_to_voigt(tensor):
                     J = mapping[(k, l)]
                     # Assign the tensor value to the Voigt matrix
                     voigt[I-1][J-1] = tensor[i-1][j-1][k-1][l-1]
-    # Return Voigt matrix
+    # Return the Voigt matrix
     return voigt
 
-# function that converts Voigt matrix notation to 4th rank stiffness tensor
+# Define a function to convert Voigt matrix notation to 3x3x3x3 stiffness tensor
 def voigt_to_tensor(voigt):
-    # Initialize an empty 4x4x4x4 tensor
-    tensor = [[[[0 for i in range(4)] for j in range(4)] for k in range(4)] for l in range(4)]
+    # Initialize an empty 3x3x3x3 tensor
+    tensor = [[[[0 for i in range(3)] for j in range(3)] for k in range(3)] for l in range(3)]
     # Define a mapping from Voigt indices to tensor indices
     mapping = {1: (1, 1), 2: (2, 2), 3: (3, 3), 4: (2, 3), 5: (1, 3), 6: (1, 2)}
     # Loop through the Voigt indices and assign the corresponding values to the tensor
@@ -35,13 +35,13 @@ def voigt_to_tensor(voigt):
             tensor[j-1][i-1][k-1][l-1] = voigt[I-1][J-1]
             tensor[i-1][j-1][l-1][k-1] = voigt[I-1][J-1]
             tensor[j-1][i-1][l-1][k-1] = voigt[I-1][J-1]
-    # Return tensor
+    # Return the tensor
     return tensor
 
 if __name__ == "__main__":
     import numpy as np
     # Test functions with an example stiffness tensor
-    tensor = [[[[1000.0 for i in range(4)] for j in range(4)] for k in range(4)] for l in range(4)]
+    tensor = [[[[1000.0 for i in range(3)] for j in range(3)] for k in range(3)] for l in range(3)]
     voigt = tensor_to_voigt(tensor)
     tensor_2 = voigt_to_tensor(voigt)
 
@@ -53,7 +53,7 @@ if __name__ == "__main__":
         print(row)
     tensor = voigt_to_tensor(voigt)
     print("Stiffness tensor:")
-    for i in range(4):
+    for i in range(3):
         print(tensor[i])
 
     print(tensor == tensor_2)
