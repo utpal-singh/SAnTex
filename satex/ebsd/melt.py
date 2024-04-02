8fb70f0fa7f3777d47b91b8846157ad5c697ef7e
utpal-singh <utpal_singh@yahoo.com>
2024-04-02 17:52:30 +1100

    materials database updated

--- satex/ebsd/melt.py
+++ satex/ebsd/melt.py
@@ -3,6 +3,11 @@ from ..tensor.tensor import Tensor
 
 
 def calcMelttensor():
+    """Reference 
+    Lee, A. L., Walker, A. M., Lloyd, G. E., & Torvela, T. (2017). 
+    Modeling the impact of melt on seismic properties during mountain building. 
+    Geochemistry, Geophysics, Geosystems, 18(3), 1090–1110. https://doi.org/10.1002/2016GC006705
+    """
     voigt_melt = np.array([[16.1,   15.967,  15.967,   0.,      0,   0.   ],
                                 [ 15.967, 16.1,   15.967,    0.,      0,   0.   ],
                                 [ 15.967,  15.967,  16.1,    0.,     0,    0.   ],
